180dcf2e5e1bef4bb4bef7bc7a3f5ebb48f9e894
Aku <akash.tvm@gmail.com>
2021-06-29 17:10:17 +0530

    testing final

--- cart/serializers.py
+++ cart/serializers.py
@@ -2,7 +2,6 @@ from .models import Cart
 from rest_framework import serializers
 #from django.contrib.auth.hashers import make_password, check_password
 #from secrets import token_hex
-#import datetime
 
 class CartListSerializer(serializers.ModelSerializer):
     class Meta:
